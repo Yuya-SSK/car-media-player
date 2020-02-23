0dfa08d930fb9a7da090f7c61a52f55fb60db62a
y-sasaki <y-sasaki@gmail.com>
2020-02-24 04:35:48 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Car Media Player Application
+
+## Android Studio
+Android Studio 4.0 Canary 9
+
+## Kotlin
+Kotlin 1.3.61
---
+++ app/src/main/java/com/ysp/camep/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,31 @@
+package com.ysp.camep.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.ysp.camep.R
+import com.ysp.camep.databinding.DashboardFragmentBinding
+import com.ysp.camep.databinding.MainActivityBinding
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var binding: DashboardFragmentBinding
+    private val dashboardViewModel: DashboardViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DashboardFragmentBinding.inflate(layoutInflater, container, false)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            binding.textDashboard.text = it
+        })
+        return binding.root
+    }
+}
